c66eead6113db59cb7d75aa765aa2a640bc4358e
Hakim bin Luqman <hakim.luqman91@gmail.com>
2013-10-30 16:32:01 +0100

    read ip headers and tcp headers

--- tcpanalyser.c
+++ tcpanalyser.c
@@ -5,10 +5,18 @@
  * To compile, use the -lpcap switch:
  * gcc -Wall -o tcpanalyser tcpanalyser.c -lpcap
  */
+#define SIZE_ETHERNET 14
 
+#include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pcap.h>
+#include <arpa/inet.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+
+#define ETHER_TYPE_IP (0x0800)
+#define ETHER_TYPE_8021Q (0x8100)
 
 int main (int argc, char *argv[]){
   char errbuff[PCAP_ERRBUF_SIZE];
@@ -20,10 +28,40 @@ int main (int argc, char *argv[]){
 
   if(pcap_open_offline(argv[1], errbuff) != NULL) {
     printf("Success! .pcap file can be opened!\n");
+    pcap_t *handle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    struct pcap_pkthdr header; // The header that pcap gives us 
+    const u_char *packet; // The actual packet 
+
+    handle = pcap_open_offline(argv[1], errbuff);
+
+    int count = 0;
+   
+    const struct ip *ip;              /* The IP header */
+    uint size_ip;
+    uint ip_len;
+
+    const struct tcphdr *tcp;            /* The TCP header */
+    uint size_tcp;
+
+    while((packet = pcap_next(handle, &header))) {
+      ip = (struct ip*)(packet + SIZE_ETHERNET);
+      size_ip = 4*ip->ip_hl;
+      ip_len = ntohs(ip->ip_len);
+      printf("\n\nIP header length: %u bytes\n", size_ip);
+      printf("Packet length: %u bytes\n", ip_len);
+
+      tcp = (struct tcphdr*)(packet + SIZE_ETHERNET + size_ip);
+      size_tcp = 4*tcp->doff;
+      printf("TCP header length: %u bytes\n", size_tcp);
+      printf("SEQ: %x\n",ntohl(tcp->seq));
+      printf("ACK: : %x\n",ntohl(tcp->ack_seq));
+    }
   }
   else{
     printf("%s\n", errbuff);
   }
-  
+
   return 0;
 }
